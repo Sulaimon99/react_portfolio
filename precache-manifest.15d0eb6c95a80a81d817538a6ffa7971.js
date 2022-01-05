9648bd23a6cea622c6bf2159c31038e79af1c5d4
Sulaimon99 <balo.oluwaseyi@gmail.com>
2022-01-05 03:53:11 +0100

    Updates

---
+++ precache-manifest.15d0eb6c95a80a81d817538a6ffa7971.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b32593f435ab0670e309",
+    "url": "/portfolio/static/css/main.e7fca0c5.chunk.css"
+  },
+  {
+    "revision": "b32593f435ab0670e309",
+    "url": "/portfolio/static/js/main.3961b727.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "69d3bc3820aaaf4832d6",
+    "url": "/portfolio/static/css/2.098f3ef9.chunk.css"
+  },
+  {
+    "revision": "69d3bc3820aaaf4832d6",
+    "url": "/portfolio/static/js/2.2667cd5a.chunk.js"
+  },
+  {
+    "revision": "9da8546fd9df2dbbc11b40be9492e55a",
+    "url": "/portfolio/index.html"
+  }
+];
